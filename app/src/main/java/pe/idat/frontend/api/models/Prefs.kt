2d9d964a32d890cdc0e5819e5459d14b67f1632b
anderssongodoy <anderssongodoygarcia@gmail.com>
2023-08-16 07:28:47 -0500

    Avance de Preferences

--- app/src/main/java/pe/idat/frontend/api/models/Prefs.kt
+++ app/src/main/java/pe/idat/frontend/api/models/Prefs.kt
@@ -5,7 +5,7 @@ import android.content.Context
 class Prefs (val context:Context){
     val SHARED_DB = "GYMDB"
     val SHARED_NOMBRE = ""
-    val SHARED_LOCATION = ""
+    val SHARED_DIRECTION = ""
     val SHARED_PRICE = ""
     val SHARED_APELLIDOS = ""
     val SHARED_DESCRIPCION = ""
@@ -15,6 +15,38 @@ class Prefs (val context:Context){
     val storage = context.getSharedPreferences(SHARED_DB, 0)
 
     fun setNombre(nombre:String){
-        storage
+        storage.edit().putString(SHARED_NOMBRE, nombre).apply()
+    }
+
+    fun getNombre():String{
+        return storage.getString(SHARED_NOMBRE, "")!!
+    }
+
+    fun setDirection(direction:String){
+        storage.edit().putString(SHARED_DIRECTION, direction).apply()
+    }
+
+    fun getDirection():String{
+        return storage.getString(SHARED_DIRECTION, "")!!
+    }
+
+    fun setPrice(price:Int){
+        storage.edit().putInt(SHARED_PRICE, price).apply()
+    }
+
+    fun getPrice(price:Int){
+        storage.getInt(SHARED_PRICE, 0)
+    }
+
+    fun setApellidos(apellidos:String){
+        storage.edit().putString(SHARED_APELLIDOS, apellidos).apply()
+    }
+
+    fun getApellidos(apellidos: String){
+        storage.getString(SHARED_APELLIDOS, "")!!
+    }
+
+    fun setDescripcion(descripcion:String){
+        storage.edit().putString(SHARED_DESCRIPCION, descripcion).apply()
     }
 }
